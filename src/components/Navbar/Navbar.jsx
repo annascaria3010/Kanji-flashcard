31c460e2ae2c9de55af910da11eb7a24d3ed5e18
annascaria3010 <annascaria01@gmail.com>
2024-12-24 17:31:34 +0900

    Header and navbar structure

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
+import './Navbar.css'
 
 const Navbar = () => {
   return (
     <div className='Nav-bar'>
-       <div>All Kanji</div> 
-       <div>Flash Cards</div> 
-       <div>Quiz</div> 
+       <div className='titles'>Home</div> 
+       <div className='titles'>Flash Cards</div> 
+       <div className='titles'>Quiz</div> 
+       <div className='titles'>Settings</div> 
     </div>
   )
 }
