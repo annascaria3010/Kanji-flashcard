1133202ff22ca49ded27c10a323049c6b26378d6
annascaria3010 <annascaria01@gmail.com>
2024-12-25 11:42:49 +0900

    UI for settings and dropdown

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,15 +1,30 @@
-import React from 'react'
-import './Navbar.css'
+import React, { useState } from 'react';
+import './Navbar.css';
 
 const Navbar = () => {
-  return (
-    <div className='Nav-bar'>
-       <div className='titles'>Home</div> 
-       <div className='titles'>Flash Cards</div> 
-       <div className='titles'>Quiz</div> 
-       <div className='titles'>Settings</div> 
-    </div>
-  )
-}
+  const [dropdownVisible, setDropdownVisible] = useState(false);
 
-export default Navbar
+  const toggleDropdown = () => {
+    setDropdownVisible((prevState) => !prevState);
+  };
+
+  return (
+    <div className="Nav-bar">
+      <div className="titles">Home</div>
+      <div className="titles">Flash Cards</div>
+      <div className="titles">Quiz</div>
+      <div className="titles settings" onClick={toggleDropdown}>
+        Settings
+        {dropdownVisible && (
+          <div className="dropdown">
+            <div className="dropdown-item" >Add</div>
+            <div className="dropdown-item">Edit</div>
+            <div className="dropdown-item">Delete</div>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
---
+++ src/flashCard.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const flashCard = () => {
+  return (
+    <div>flashCard</div>
+  )
+}
+
+export default flashCard
