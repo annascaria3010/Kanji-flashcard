ac6b8acaf3f2e3c7225e2edc363dd858990de0cd
annascaria3010 <annascaria01@gmail.com>
2024-12-26 17:31:15 +0900

    flashcard

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,31 +1,14 @@
-import React, { useState } from 'react';
-import { Link, useNavigate } from 'react-router-dom';
+import React from 'react';
+import { Link } from 'react-router-dom';
 import './Navbar.css';
 
 const Navbar = () => {
-  const [showDropdown, setShowDropdown] = useState(false);
-  const navigate = useNavigate();
-
-  const handleDropdownClick = (action) => {
-    setShowDropdown(false); // Close the dropdown
-    navigate(`/settings/${action}`); // Navigate to the appropriate settings page
-  };
-
   return (
     <div className="Nav-bar">
       <Link to="/" className="titles">Home</Link>
       <Link to="/flash-cards" className="titles">Flash Cards</Link>
       <Link to="/quiz" className="titles">Quiz</Link>
-      <div className="titles dropdown">
-        <span className="titles">Settings</span>
-        {showDropdown && (
-          <div className="dropdown-menu">
-            <div onClick={() => handleDropdownClick('add')}>Add</div>
-            <div onClick={() => handleDropdownClick('edit')}>Edit</div>
-            <div onClick={() => handleDropdownClick('delete')}>Delete</div>
-          </div>
-        )}
-      </div>
+      <Link to="/settings" className="titles">Settings</Link>
     </div>
   );
 };
--- src/flashCard.jsx
+++ src/flashCard.jsx
@@ -1,15 +1,54 @@
-import React from 'react'
-import Header from './components/Header/Header'
-import Navbar from './components/Navbar/Navbar'
+import React, { useState } from 'react';
+import Header from './components/Header/Header';
+import Navbar from './components/Navbar/Navbar';
+import './flashCard.css'
 
-const flashCard = () => {
-  return (
-    <div className='Home'>
-    <Header/>
-    <Navbar/>
-     <h1>Flash Card</h1>
-     </div>
-  )
-}
+const FlashCard = () => {
 
-export default flashCard
+  const kanjiList = [
+    "一", "二", "三", "四", "五", "六", "七", "八", "九", "十",
+    "人", "今", "日", "週", "月", "年", "中", "山", "川", "左",
+    "右", "大", "木", "本", "水", "火", "父", "母", "耳", "口",
+    "休", "何", "入", "上", "前", "下", "千", "午", "友", "古",
+    "名", "国", "外", "女", "男", "子", "学", "生", "小", "書",
+    "毎", "先", "会", "万", "円", "出", "分", "北", "半", "南",
+    "土", "多", "天", "安", "少", "店", "後", "手", "新", "時",
+    "来", "東", "校", "気", "白", "百", "目", "社", "空", "立",
+    "聞", "花", "行", "西", "見", "言", "話", "語", "読", "買",
+    "足", "車", "道", "金", "長", "間", "雨", "電", "食", "飲",
+    "駅", "高", "魚"
+  ];
+
+  const [currentKanji, setCurrentKanji] = useState("");
+
+  // Function to get random Kanji characters
+  const getRandomKanji = () => {
+    const randomIndex = Math.floor(Math.random() * kanjiList.length);
+    return kanjiList[randomIndex];
+  };
+
+  // Set random Kanji on component mount
+  React.useEffect(() => {
+    setCurrentKanji(getRandomKanji());
+  }, []);
+
+  const handleNextClick = () => {
+    setCurrentKanji(getRandomKanji());
+  };
+
+   return (
+    <div className="Home">
+      <Header />
+      <Navbar />
+      <h1>Flash Card</h1>
+      <div className="flashcard-container">
+        <div className="flashcard">{currentKanji}</div>
+      </div>
+      <button className="next-button" onClick={handleNextClick}>
+        Next
+      </button>
+    </div>
+  );
+};
+
+export default FlashCard;
